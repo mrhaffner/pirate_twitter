db5e58ca18f6e6badd295fec0ce4c00b4773e96d
mrhaffner <mrhaffner13@gmail.com>
2022-09-02 16:57:54 -0500

    sign up now defaults to user level authorization

--- api/src/main/java/com/piritter/api/controller/AuthController.java
+++ api/src/main/java/com/piritter/api/controller/AuthController.java
@@ -80,40 +80,14 @@ public class AuthController {
           .body(new MessageResponse("Error: Username is already taken!"));
     }
 
-    // Create new user's account
     User user = new User(signUpRequest.getUsername(), 
-               encoder.encode(signUpRequest.getPassword()));
+            encoder.encode(signUpRequest.getPassword()));
 
-    Set<String> strRoles = signUpRequest.getRole();
     Set<Role> roles = new HashSet<>();
-
-    if (strRoles == null) {
-      Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-          .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-      roles.add(userRole);
-    } else {
-      strRoles.forEach(role -> {
-        switch (role) {
-        case "admin":
-          Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-          roles.add(adminRole);
-
-          break;
-        case "mod":
-          Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-          roles.add(modRole);
-
-          break;
-        default:
-          Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-          roles.add(userRole);
-        }
-      });
-    }
-
+    Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        
+    roles.add(userRole);
     user.setRoles(roles);
     userRepository.save(user);
 
--- api/src/main/java/com/piritter/api/payload/request/SignupRequest.java
+++ api/src/main/java/com/piritter/api/payload/request/SignupRequest.java
@@ -1,7 +1,5 @@
 package com.piritter.api.payload.request;
 
-import java.util.Set;
-
 import javax.validation.constraints.*;
 
 import lombok.Data;
@@ -12,8 +10,6 @@ public class SignupRequest {
   @NotBlank
   @Size(min = 3, max = 20)
   private String username;
-
-  private Set<String> role;  // should not have this, default is user
 
   @NotBlank
   @Size(min = 6, max = 40)
