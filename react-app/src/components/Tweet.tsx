536bd2109fe0d5a485770e19d494aa45fb006ef8
mrhaffner <mrhaffner13@gmail.com>
2022-09-10 20:25:04 -0500

    added tweet style

--- react-app/src/components/Tweet.tsx
+++ react-app/src/components/Tweet.tsx
@@ -11,22 +11,27 @@ const Tweet = ({ tweet }: Props) => {
   const { id, datetime, content, user } = tweet;
   // needs to generate updated datetime
   return (
-    <div>
-      <div>
+    <div className="border border-black no-overlap flex">
+      <div className="m-2">
         <Avatar seed={user.id} />
       </div>
-      <div>
-        <div>
+      <div className="my-2 space-y-0.5 w-full">
+        <div className="flex space-x-2">
           <a href="#">
             <span>@{user.handle}</span>
           </a>
+          <div className="-my-1">.</div>
           <div>{datetime}</div>
         </div>
         <div>
           <div>{content}</div>
-          <div>
-            <RetweetButton tweetId={id} />
-            <LikeButton tweet={tweet} />
+          <div className="flex mt-2">
+            <div className="w-36">
+              <RetweetButton tweetId={id} />
+            </div>
+            <div>
+              <LikeButton tweet={tweet} />
+            </div>
           </div>
         </div>
       </div>
