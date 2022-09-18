32cf7d033a0855b635f01f674220504ab7cf9b05
mrhaffner <mrhaffner13@gmail.com>
2022-09-17 21:24:41 -0500

    added user service

---
+++ react-app/src/service/userService.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+const baseUri = 'http://127.0.0.1:8080/api/user';
+
+// needs return type
+export async function follow(username: string, token: string) {
+  try {
+    const response = await axios.put(
+      `${baseUri}/${username}/follow`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    // return a boolean based on response code
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+// needs return type
+export async function unfollow(username: string, token: string) {
+  try {
+    const response = await axios.put(
+      `${baseUri}/${username}/unfollow`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    // return a boolean based on response code
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+// needs return type
+export async function searchUsernames(partialUsername: string) {
+  try {
+    const response = await axios.get(
+      `${baseUri}/find-similar/${partialUsername}`,
+    );
+    return response.data;
+  } catch (error) {
+    return [];
+  }
+}
